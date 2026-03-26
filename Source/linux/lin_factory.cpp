ccf1b3673d3e1398f24c2dd5b225e24c437b3480
tip-aaron <qaamragudos@tip.edu.ph>
2026-03-26 20:38:19 +0800

    make headers

---
+++ Source/linux/lin_factory.cpp
@@ -0,0 +1,10 @@
+#include <os_factory.hpp>
+
+std::unique_ptr<IScheduler> OsSimulationFactory::createScheduler() {
+
+}
+
+std::unique_ptr<IMemoryManager> OsSimulationFactory::createMemoryManager() {
+
+}
+
