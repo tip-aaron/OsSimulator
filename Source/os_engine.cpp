5e12083d898ae155500a43d850280b5d43cbece6
tip-aaron <qaamragudos@tip.edu.ph>
2026-03-28 12:36:23 +0800

    add os simulation engine

--- Source/os_engine.cpp
+++ Source/os_engine.cpp
@@ -9,6 +9,26 @@ os_simulation_engine::OsSimulationEngine::OsSimulationEngine(
 void os_simulation_engine::OsSimulationEngine::runSimulation() {
   while (!mScheduler->isFinished()) {
     auto *pRunningProcess = mScheduler->getNextProcessToRun();
+
+    if (pRunningProcess != nullptr) {
+      os_simulation_process::TraceAccess nextTraceAccess =
+          getNextTraceForProcess(pRunningProcess->getId());
+      bool isHit = mMemoryManager->accessAddress(
+          pRunningProcess->getId(), nextTraceAccess.mVirtualAddress,
+          nextTraceAccess.mAccessType);
+
+      if (!isHit) {
+        mMemoryManager->handlePageFault(pRunningProcess->getId(),
+                                        nextTraceAccess.mVirtualAddress,
+                                        nextTraceAccess.mAccessType);
+        pRunningProcess->block(
+            os_simulation_architecture::BACKING_STORE_LATENCY_MS);
+      } else {
+        mScheduler->executeProcess(pRunningProcess);
+      }
+    }
+
+    mScheduler->addTick();
   }
 }
 
